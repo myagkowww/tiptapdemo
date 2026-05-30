7ba5b5cb63cf3d26807c8d3208d042fc6e1d2e87
egor <myagkovev@gmail.com>
2026-05-30 14:26:42 +0500

    myagkow: netify plugin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export', // Говорит Next.js собрать статику
+    images: {
+        unoptimized: true, // Необходимо для статического экспорта
+    },
+    basePath: '/tiptapdemo', // ВАЖНО: это имя твоего репозитория на GitHub (myagkowww/tiptapdemo)
 };
 
 export default nextConfig;
